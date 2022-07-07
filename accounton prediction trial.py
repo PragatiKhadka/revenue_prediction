5ecfda912cc3b1538bdb5a56326d77fff9af6f37
JorgVervaet <103995712+JorgVervaet@users.noreply.github.com>
2022-07-07 16:32:11 +0200

    07/07/2022

--- accounton prediction trial.py
+++ accounton prediction trial.py
@@ -1,4 +1,3 @@
-import json
 import pandas as pd
 import numpy as np
 from pandas import json_normalize
@@ -8,28 +7,14 @@ import matplotlib.pyplot as plt
 # Opening JSON file
 f = open('/Users/Jorg/Accounton data/clean_accounton.csv')
   
-# returns JSON object as 
-# a dictionary
-#raw_data = json.load(f)
-
 df1 = pd.read_csv(f)
 
 
 X = df1[['ebit.2020', 'ebit.2019', 'ebit.2018', 'ebit.2017', 'ebit.2016',
-       'ebit.2015', 'total_assets.2020',
-       'total_assets.2019', 'total_assets.2018', 'total_assets.2017',
-       'total_assets.2016', 'total_assets.2015', 'total_liabilities.2020',
+       'ebit.2015', 'total_liabilities.2020',
        'total_liabilities.2019', 'total_liabilities.2018',
        'total_liabilities.2017', 'total_liabilities.2016',
-       'total_liabilities.2015', 'operating_profit_and_loss.2020',
-       'operating_profit_and_loss.2019', 'operating_profit_and_loss.2018',
-       'operating_profit_and_loss.2017', 'operating_profit_and_loss.2016',
-       'operating_profit_and_loss.2015', 'financial_profit_and_loss.2020',
-       'financial_profit_and_loss.2019', 'financial_profit_and_loss.2018',
-       'financial_profit_and_loss.2017', 'financial_profit_and_loss.2016',
-       'financial_profit_and_loss.2015', 'staff_count.2020',
-       'staff_count.2019', 'staff_count.2018', 'staff_count.2017',
-       'staff_count.2016', 'staff_count.2015',
+       'total_liabilities.2015',
        'staff_costs.2020', 'staff_costs.2019', 'staff_costs.2018',
        'staff_costs.2016', 'staff_costs.2017', 'staff_costs.2015']]
 y = df1[['revenue.2020', 'revenue.2019', 'revenue.2018', 'revenue.2017', 'revenue.2016', 'revenue.2015']]
@@ -60,14 +45,14 @@ x_poly= poly_regs.fit_transform(X)
 lin_reg_2 =linear_model.LinearRegression()  
 lin_reg_2.fit(x_poly, y)  
 
-#plt.scatter(X,y,color="blue")  
-#plt.plot(X, lin_reg_2.predict(poly_regs.fit_transform(X)), color="red")  
-#plt.title("Bluff detection model(Polynomial Regression)")  
-#plt.xlabel("Features")  
-#plt.ylabel("Revenue")  
+plt.scatter(X,y,color="blue")  
+plt.plot(X, lin_reg_2.predict(poly_regs.fit_transform(X)), color="red")  
+plt.title("Bluff detection model(Polynomial Regression)")  
+plt.xlabel("Features")  
+plt.ylabel("Revenue")  
 #plt.show()  
 
-sns.residplot(data=df1, x='ebit.2018', y='revenue.2018', order=2)
+#sns.residplot(data=df1, x='ebit.2018', y='revenue.2018', order=2)
 
 plt.show()
 
